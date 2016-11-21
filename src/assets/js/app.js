3147f664bba06998ebd20087b6dbdd8bb1cfb176
Maden-maxi <maden.maxi@yandex.ru>
2016-11-21 02:31:41 +0200

    Adding Flickity slider

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,16 @@
 $(document).foundation();
+$(document).ready(function () {
+    $(".members-carousel").flickity({
+        cellAlign: "left",
+        pageDots: false,
+        contain: true,
+        wrapAround: false,
+        autoPlay: 2000,
+        arrowShape: {
+            x0: 20,
+            x1: 55, y1: 40,
+            x2: 70, y2: 40,
+            x3: 35
+        }
+    });
+});
